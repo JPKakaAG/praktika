75317bcd4be6770ddab7d7036e1e47fccd572ea7
Вадим <@DESKTOP-SBB1HEM>
2024-02-14 03:15:10 +0300

    sdf

--- praktika/praktika/N4.xaml.cs
+++ praktika/praktika/N4.xaml.cs
@@ -20,10 +20,33 @@ namespace praktika
     /// </summary>
     public partial class N4 : Window
     {
+
+        int[,] matrix;
         public N4()
         {
             InitializeComponent();
+            dg1.CellEditEnding += Dg1_CellEditEnding;
         }
+
+        private void Dg1_CellEditEnding(object sender, DataGridCellEditEndingEventArgs e)
+        {       
+            int rowIndex = e.Row.GetIndex();
+            int columnIndex = e.Column.DisplayIndex;         
+            matrix[rowIndex, columnIndex] = Convert.ToInt32(((TextBox)e.EditingElement).Text);
+            if (int.TryParse(tb2.Text, out int matrixRows) &&
+                int.TryParse(tb3.Text, out int matrixColumns) &&
+                int.TryParse(tbA.Text, out int a) &&
+                int.TryParse(tbB.Text, out int b))
+            {               
+                int[] resultArray = class2.GenerateArray(matrix, a, b);
+                tb4.Text = $"Массив: {string.Join(", ", resultArray)}";
+            }
+            else
+            {
+                MessageBox.Show("Введите корректные числа.");
+            }          
+        }
+
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             if (int.TryParse(tb2.Text, out int matrixRows) &&
@@ -31,19 +54,15 @@ namespace praktika
                 int.TryParse(tbA.Text, out int a) &&
                 int.TryParse(tbB.Text, out int b))
             {
-                int[,] matrix = class2.GenerateMatrix(matrixRows, matrixColumns);
+                matrix = class2.GenerateMatrix(matrixRows, matrixColumns);
                 dg1.ItemsSource = Class1.ToDataTable(matrix).DefaultView;
                 int[] resultArray = class2.GenerateArray(matrix, a, b);
-
                 tb4.Text = $"Массив: {string.Join(", ", resultArray)}";
             }
             else
             {
                 MessageBox.Show("Введите корректные числа.");
-            }
-
-           
-            
+            }   
         }
 
         private void back_click(object sender, RoutedEventArgs e)
